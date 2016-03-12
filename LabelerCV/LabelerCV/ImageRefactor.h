dbae58111c090a4f13c0d01a13bb73351ca63873
RcRonco <cohenronco@gmail.com>
2016-03-12 14:02:23 +0200

    Command line switches added

--- LabelerCV/LabelerCV/ImageRefactor.h
+++ LabelerCV/LabelerCV/ImageRefactor.h
@@ -3,7 +3,7 @@
 
 #include <boost\filesystem.hpp>
 #include <stdint.h>
-#include <opencv.hpp>
+
 
 namespace fs = boost::filesystem;
 
@@ -11,72 +11,8 @@ namespace Labeler
 {
 	namespace ImageRefactor
 	{
-		void CopyDir(fs::path inpath, fs::path outpath, uint32_t depth = 0);
-
-		void ResizeImages(fs::path path, uint32_t width, uint32_t height)
-		{
-			if (fs::exists(path) && fs::is_directory(path))
-			{
-				fs::directory_iterator end_iter;
-					
-				for (fs::directory_iterator dir_iter = fs::directory_iterator(path); dir_iter != end_iter; ++dir_iter)
-				{
-					ResizeImages(dir_iter->path(), width, height);
-				}
-			}
-			else
-			{
-				cv::Mat tempMat = cv::imread(path.string(), 1);
-				cv::resize(tempMat.clone(), tempMat, cv::Size(width, height));
-				cv::imwrite(path.string(), tempMat);
-			}
-		}	
-		void ResizeImages(fs::path inpath, fs::path outpath, uint32_t width, uint32_t height)
-		{
-			if (!fs::exists(inpath))
-				return;
-
-			if (fs::is_directory(inpath))
-				CopyDir(inpath, outpath);
-			else
-			{
-				fs::create_directory(outpath);
-				outpath = outpath.append(inpath.filename().c_str()).append(inpath.extension().c_str());
-				fs::copy_file(inpath, outpath);
-			}
-
-			ResizeImages(outpath, width, height);
-		}
-
-		void CopyDir(fs::path inpath, fs::path outpath, uint32_t depth)
-		{
-			fs::path workPath = outpath;
-
-			if (depth != 0)
-				workPath.append(inpath.filename().c_str());
-			if (fs::exists(inpath))
-			{
-				if (fs::is_directory(inpath))
-				{
-					fs::directory_iterator end_iter;
-					fs::create_directories(workPath);
-
-					for (fs::directory_iterator dir_iter = fs::directory_iterator(inpath); dir_iter != end_iter; ++dir_iter)
-					{
-						CopyDir(dir_iter->path(), workPath, depth + 1);
-						std::wclog << "Current depth: " << depth << " - " << workPath.c_str() << std::endl;
-					}
-				}
-				else if (!fs::exists(workPath))
-				{
-					fs::copy_file(inpath, workPath);
-				}
-				else
-				{
-					std::wcerr << "The file " << workPath.c_str() << " already exists, override files not supported." << std::endl;
-				}
-			}
-		}
+		void ResizeImages(fs::path path, uint32_t width, uint32_t height);
+		void ResizeImages(fs::path inpath, fs::path outpath, uint32_t width, uint32_t height);
 	};
 };
 
