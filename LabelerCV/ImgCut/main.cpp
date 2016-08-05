f81482a3b51e6fe951bb0a33aed3f5cfa45c1424
RcRonco <roncohen04@gmail.com>
2016-08-05 12:35:54 +0300

    * Support Linux and Windows 7 or later. * Change the structure to multiple tools instead of one with alot of flags. * Move to CMake instead of visual studio project.

---
+++ LabelerCV/ImgCut/main.cpp
@@ -0,0 +1,48 @@
+//
+// Created by ronco on 7/1/16.
+//
+#include <stdint.h>
+#include <iostream>
+#include <boost/program_options.hpp>
+
+#include "ImageCut.h"
+
+namespace po = boost::program_options;
+void runVideoPlayer(std::string path, std::string outputPath);
+
+int main(int argc, const char** argv) {
+    po::variables_map vm;
+
+    try {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+                ("help,?", "Show help message.")
+                ("img,i", po::value<void>(), "Cut image.")
+                ("video,v", po::value<std::string>(), "Cut image from specific time in video., Video Path")
+                ("width,w", po::value<uint32_t>(), "Width of the object in the image.")
+                ("height,h", po::value<uint32_t>(), "Height of the object in the image.")
+                ("x", po::value<uint32_t>(), "X coordinate of the object in the image.")
+                ("y", po::value<uint32_t>(), "Y coordinate of the object in the image.")
+                ("output,o", po::value<std::string>(), "Output file for the train data.");
+
+        po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
+        if (!vm["help"].empty()) {
+            std::cout << desc;
+        } else if (vm["y"].empty() && vm["height"].empty() &&
+                   vm["x"].empty() && vm["width"].empty() &&
+                   vm["output"].empty() && (vm["video"].empty() || vm["img"].empty())) {
+            std::cout << "Please enter all the needed parameters" << std::endl;
+        } else {
+            if (!vm["img"].empty()) {
+                CutImage(vm["img"].as<std::string>(), vm["x"].as<uint32_t>(),
+                         vm["y"].as<uint32_t>(), vm["width"].as<uint32_t>(),
+                         vm["height"].as<uint32_t>(), vm["output"].as<std::string>());
+            } else if (!vm["video"].empty() && !vm["time"].empty()) {
+
+            }
+        }
+    } catch (std::exception& e) {
+        std::cout << "An error occurred: " << e.what() << std::endl;
+    }
+}
+
---
+++ LabelerCV/VideoPlayer/VideoPlayer.h
@@ -0,0 +1,78 @@
+#ifndef _RC_VIDEO_PLAYER_H
+#define _RC_VIDEO_PLAYER_H
+
+#include <stdint.h>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+#include <vector>
+
+namespace Labeler
+{
+	enum class LabelType { Human, Car, Animal };
+
+	const cv::Scalar BLUE(255, 0, 0);
+	const cv::Scalar GREEN(0, 255, 0);
+	const cv::Scalar RED(0, 0, 255);
+
+	using LabeledRect = std::pair<cv::Rect, LabelType>;
+	static const char* szVideoDescription = "Run Dataset video player\nSPACE - Stop/Start the video.\nBACK - Remove last label. (Undo)\nESCAPE - Exit\nS\\s - Save image to train format.\n1\\T\\t - Set label to Human.\n2\\R\\r - Set label to Car.\n3\\C\\c - Set label to Animal.";
+	class VideoPlayer
+	{
+		private:
+			const char* _TIMEBAR_NAME = "Timeline";
+			const char* _WIN_NAME;
+			std::string _path;
+			std::ofstream _ofsTrainVal;
+			cv::Mat _foregroundMat;
+			std::vector<LabeledRect> _historyRects;
+			cv::Mat _frameBuffer;
+			cv::VideoCapture _capture;
+			uint32_t fps, frame_count, vidlength, show_interval;
+			uint32_t _frameCounter = 0, cropIndex = 0;
+			cv::Point point1, point2;
+			bool drag = false;
+			LabelType label = LabelType::Human;
+
+		public:
+			bool isPlaying = false;
+			bool Shutdown = false;
+
+		public:
+			VideoPlayer(std::string videoPath, const char * winname, std::string outputPath = "Full/trainval.txt");
+			void run();
+			bool isVideoEnded() noexcept;
+			void changeTime(int seconds = 0) noexcept;
+			bool readImage() noexcept;
+			void showImage();
+			void CutImages();
+			void SaveImage();
+			void getbackMat();
+			void keyAction(char key);
+			void pushRect(LabeledRect& rect) noexcept;
+
+			// Access Methods
+			cv::Mat getFrame() noexcept { return _frameBuffer; }
+			cv::Mat getForegroundImage() noexcept { return _foregroundMat; }
+
+			bool isMouseDragging() noexcept { return drag; }
+			void setMouseDragging(bool state) noexcept { drag = state; }
+
+			cv::Point getPoint1() noexcept { return point1; }
+			void setPoint1(cv::Point point) noexcept { point1 = point; }
+
+			cv::Point getPoint2() noexcept { return point2; }
+			void setPoint2(cv::Point point) noexcept { point2 = point; }
+
+			void setLabel(LabelType lbtype) noexcept { label = lbtype; }
+			LabelType getLabel() noexcept { return label; }
+
+			uint64_t  getCropCounter() noexcept { return cropIndex; }
+			const char* getWindowName() const noexcept { return _WIN_NAME; }
+
+		private:
+			bool loadVideo(std::string VideoPath);
+
+	};
+};
+
+#endif // !_RC_VIDEO_PLAYER_H
